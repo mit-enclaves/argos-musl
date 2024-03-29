9e766b4c3787b5d3fd9f9294c299cbf1f556cd06
CharlyCst <charly.castes@epfl.ch>
2024-03-29 11:27:43 +0100

    Remove syscall for thread local storage initialization

--- src/env/__init_tls.c
+++ src/env/__init_tls.c
@@ -15,11 +15,12 @@ int __init_tp(void *p)
 {
 	pthread_t td = p;
 	td->self = td;
-	int r = __set_thread_area(TP_ADJ(p));
-	if (r < 0) return -1;
-	if (!r) libc.can_do_threads = 1;
+    __asm__ __volatile__ ("wrfsbase %0" : : "r" (p));
+	/* int r = __set_thread_area(TP_ADJ(p)); */
+	/* if (r < 0) return -1; */
+	/* if (!r) libc.can_do_threads = 1; */
 	td->detach_state = DT_JOINABLE;
-	td->tid = __syscall(SYS_set_tid_address, &__thread_list_lock);
+	/* td->tid = __syscall(SYS_set_tid_address, &__thread_list_lock); */
 	td->locale = &libc.global_locale;
 	td->robust_list.head = &td->robust_list.head;
 	td->sysinfo = __sysinfo;
