80fabd383271b46056840d03b39047e8bc79558c
Jules Drean <drean@mit.edu>
2024-07-08 21:25:54 +0000

    Fix bug to let MUSL use buffering for stoud. In general, let MUSL handle complexity and only overwrite syscall implementations

--- src/stdio/stdout.c
+++ src/stdio/stdout.c
@@ -3,11 +3,6 @@
 
 #undef stdout
 
-size_t _tyche_stdout_write(FILE *f, const unsigned char *buf, size_t len) {
-    tyche_write(f->fd, buf, len);
-    return len;
-}
-
 static unsigned char buf[BUFSIZ+UNGET];
 hidden FILE __stdout_FILE = {
 	.buf = buf+UNGET,
@@ -15,11 +10,7 @@ hidden FILE __stdout_FILE = {
 	.fd = 1,
 	.flags = F_PERM | F_NORD,
 	.lbf = '\n',
-#ifdef TYCHE_NO_SYSCALL
-    .write = _tyche_stdout_write,
-#else
 	.write = __stdout_write,
-#endif
 	.seek = __stdio_seek,
 	.close = __stdio_close,
 	.lock = -1,
