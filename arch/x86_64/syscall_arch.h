5d6f52457624a41b69b0fe336d15551405c77cb5
Jules Drean <drean@mit.edu>
2024-07-11 18:26:13 +0000

    Use syscalls

--- arch/x86_64/syscall_arch.h
+++ arch/x86_64/syscall_arch.h
@@ -4,69 +4,69 @@
 
 static __inline long __syscall0(long n)
 {
-	return tyche_syscall(n,0,0,0,0,0,0);
-	//unsigned long ret;
-	//__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n) : "rcx", "r11", "memory");
-	//return ret;
+	//return tyche_syscall(n,0,0,0,0,0,0);
+	unsigned long ret;
+	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n) : "rcx", "r11", "memory");
+	return ret;
 }
 
 static __inline long __syscall1(long n, long a1)
 {
-	return tyche_syscall(n,a1,0,0,0,0,0);
-	//unsigned long ret;
-	//__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1) : "rcx", "r11", "memory");
-	//return ret;
+	//return tyche_syscall(n,a1,0,0,0,0,0);
+	unsigned long ret;
+	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1) : "rcx", "r11", "memory");
+	return ret;
 }
 
 static __inline long __syscall2(long n, long a1, long a2)
 {
-	return tyche_syscall(n,a1,a2,0,0,0,0);
-	//unsigned long ret;
-	//__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2)
-	//					  : "rcx", "r11", "memory");
-	//return ret;
+	//return tyche_syscall(n,a1,a2,0,0,0,0);
+	unsigned long ret;
+	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2)
+						  : "rcx", "r11", "memory");
+	return ret;
 }
 
 static __inline long __syscall3(long n, long a1, long a2, long a3)
 {
-	return tyche_syscall(n,a1, a2, a3,0,0,0);
-	// unsigned long ret;
-	// __asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
-	// 					  "d"(a3) : "rcx", "r11", "memory");
-	// return ret;
+	//return tyche_syscall(n,a1, a2, a3,0,0,0);
+	unsigned long ret;
+	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
+						  "d"(a3) : "rcx", "r11", "memory");
+	return ret;
 }
 
 static __inline long __syscall4(long n, long a1, long a2, long a3, long a4)
 {
-	return tyche_syscall(n,a1, a2, a3, a4,0,0);
-	// unsigned long ret;
-	// register long r10 __asm__("r10") = a4;
-	// __asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
-	// 					  "d"(a3), "r"(r10): "rcx", "r11", "memory");
-	// return ret;
+	//return tyche_syscall(n,a1, a2, a3, a4,0,0);
+	unsigned long ret;
+	register long r10 __asm__("r10") = a4;
+	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
+						  "d"(a3), "r"(r10): "rcx", "r11", "memory");
+	return ret;
 }
 
 static __inline long __syscall5(long n, long a1, long a2, long a3, long a4, long a5)
 {
-	return tyche_syscall(n, a1, a2, a3, a4, a5,0);
-	// unsigned long ret;
-	// register long r10 __asm__("r10") = a4;
-	// register long r8 __asm__("r8") = a5;
-	// __asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
-	// 					  "d"(a3), "r"(r10), "r"(r8) : "rcx", "r11", "memory");
-	// return ret;
+	//return tyche_syscall(n, a1, a2, a3, a4, a5,0);
+	unsigned long ret;
+	register long r10 __asm__("r10") = a4;
+	register long r8 __asm__("r8") = a5;
+	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
+						  "d"(a3), "r"(r10), "r"(r8) : "rcx", "r11", "memory");
+	return ret;
 }
 
 static __inline long __syscall6(long n, long a1, long a2, long a3, long a4, long a5, long a6)
 {
-	return tyche_syscall(n,a1, a2, a3, a4, a5, a6);
-	// unsigned long ret;
-	// register long r10 __asm__("r10") = a4;
-	// register long r8 __asm__("r8") = a5;
-	// register long r9 __asm__("r9") = a6;
-	// __asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
-	// 					  "d"(a3), "r"(r10), "r"(r8), "r"(r9) : "rcx", "r11", "memory");
-	// return ret;
+	//return tyche_syscall(n,a1, a2, a3, a4, a5, a6);
+	unsigned long ret;
+	register long r10 __asm__("r10") = a4;
+	register long r8 __asm__("r8") = a5;
+	register long r9 __asm__("r9") = a6;
+	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
+						  "d"(a3), "r"(r10), "r"(r8), "r"(r9) : "rcx", "r11", "memory");
+	return ret;
 }
 
 #define VDSO_USEFUL
--- src/misc/syscall.c
+++ src/misc/syscall.c
@@ -18,5 +18,6 @@ long syscall(long n, ...)
 	e=va_arg(ap, syscall_arg_t);
 	f=va_arg(ap, syscall_arg_t);
 	va_end(ap);
-	return __syscall_ret(tyche_syscall(n,a,b,c,d,e,f));
+	//return __syscall_ret(tyche_syscall(n,a,b,c,d,e,f));
+	return __syscall_ret(__syscall(n,a,b,c,d,e,f));
 }
